56cf26f6f5cfb3283eca505e2ee44cb3d57b0212
Harshraj305 <1700harshraj@gmail.com>
2022-11-05 22:14:02 +0530

    done

--- bmiCalculator.js
+++ bmiCalculator.js
@@ -9,3 +9,21 @@ app.use(bodyParser.urlencoded({
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/bmiCalculator.html');
 })
+
+app.post('/', function(req, res) {
+
+    var weight = Number(req.body.weight); 
+    var height = Number(req.body.height); 
+    
+    var result = (weight)/(height * height) * 10000;
+
+    res.send('The calculated BMI will be : ' + result);
+})
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/bmiCalculator.html');
+});
+
+app.listen(3000, function() {
+    console.log('Listening to port 3000 ....');
+})
